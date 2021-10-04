22eaf9e24877086b216c7b37833585c4b227833c
Darielly Santos <daryh_cristina@hotmail.com>
2021-10-04 20:46:04 -0300

    feat: incluido uma verificacao para evitar divisao por numero negativo

--- funcao-divisao/divisao.js
+++ funcao-divisao/divisao.js
@@ -1,10 +1,15 @@
 function divisao(x, y) {
+  if (y < 0) {
+    return new Error("ERROR: Divisão por número negativo");
+  }
+
   const z = x / y;
+
   return z;
 }
 
 function main() {
-  let resultado = divisao(4, 6);
+  let resultado = divisao(4, -3);
 
   console.log(resultado);
 }
