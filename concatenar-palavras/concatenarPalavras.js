f374155d6f0b4b77eb0320fea577e9d95a226124
Darielly Santos <daryh_cristina@hotmail.com>
2021-10-04 21:11:15 -0300

    feat: criada funcao para concatenar palavras

---
+++ concatenar-palavras/concatenarPalavras.js
@@ -0,0 +1,10 @@
+function concatenarPalavras(x, y) {
+  return x + y;
+}
+
+function main() {
+  let resultado = concatenarPalavras("Olá ", "mundo!");
+  return console.log(resultado);
+}
+
+main();
