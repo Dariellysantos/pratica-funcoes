bd649f63e4134e73ee5bbe02802be3330cd802f8
Darielly Santos <daryh_cristina@hotmail.com>
2021-10-05 14:52:47 -0300

    Aplicada condiçoes para o resultado e criaçao de  erros

--- remover-palavras/removerPalavra.js
+++ remover-palavras/removerPalavra.js
@@ -1,15 +1,33 @@
 function removerPalavraDaFrase(frase, palavraARemover) {
+  if (frase == "") {
+    return Error("A frase recebida é inválida: vazia");
+  }
+  if (palavraARemover == "") {
+    return Error("A palavra a ser encontrada é inválida: vazia");
+  }
+  if (frase == isNaN) {
+    return Error("A frase recebida é inválida: um número!");
+  }
+  if (palavraARemover == isNaN) {
+    return Error("A palavra a ser encontrada é inválida: um número");
+  }
+
   let listaFrase = frase.split(" ");
   let indiceDaPlavra = listaFrase.indexOf(palavraARemover);
+  if (indiceDaPlavra < 0) {
+    return Error("A palavra a ser removida é inválida: vazia");
+  }
+
   listaFrase.splice(indiceDaPlavra, 1);
   return listaFrase.join(" ");
 }
 
 function main() {
   let resultado = removerPalavraDaFrase(
-    "Batatinha quando nasce se esparrama pelo chão",
+    "Batatinha quando nasce se esparama pelo chão",
     "quando"
   );
+
   return console.log(resultado);
 }
 main();
