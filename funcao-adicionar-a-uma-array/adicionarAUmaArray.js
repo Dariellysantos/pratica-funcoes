349364d406671fecd31e6d269b30a1fc647d85fa
Darielly Santos <daryh_cristina@hotmail.com>
2021-10-08 15:27:34 -0300

    feat: adcionei condicoes aos valores recebidos nos parametros

--- funcao-adicionar-a-uma-array/adicionarAUmaArray.js
+++ funcao-adicionar-a-uma-array/adicionarAUmaArray.js
@@ -1,11 +1,17 @@
-function adicionarPalavraArray(palavras, palavraAadicionar) {
-  palavras.push(palavraAadicionar);
+function adicionarPalavraArray(palavras, palavraAAdicionar) {
+  if (palavraAAdicionar === "") {
+    return Error("A palavra a ser adicionada é inválida: vazia");
+  }
+  if (!isNaN(palavraAAdicionar)) {
+    return Error("A palavra a ser adicionada é inválida: um número");
+  }
+
+  palavras.push(palavraAAdicionar);
   return palavras;
 }
 
 function main() {
-  let lista = "maça,pera";
-  let resultado = adicionarPalavraArray(lista.split(","), "caqui");
+  let resultado = adicionarPalavraArray(["jaboticaba", "morango"], "caqui");
 
   console.log(resultado);
 }
